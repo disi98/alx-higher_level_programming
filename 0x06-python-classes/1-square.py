532a1e6b412443017598113baa19464f6ceabb4f
Albert Disi <disialbert1998@gmail.com>
2022-01-04 19:54:28 +0300

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initializing the class square using the __init__ method
+        Args:
+            size: Is the size of the square"""
+        self.__size = size
